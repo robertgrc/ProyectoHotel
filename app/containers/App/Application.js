07d28f9647c6ded984d920545d74e568aa6b0eb4
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-03-14 17:36:40 -0400

    Create AuthProvider

--- app/containers/App/Application.js
+++ app/containers/App/Application.js
@@ -26,43 +26,45 @@ import FormularioReserva from '../Pages/FormularioReserva/FormularioReserva';
 import ControlCuenta from '../Pages/ControlCuenta/ControlCuenta';
 import DiarioIngresos from '../Pages/DiarioIngresos/DiarioIngresos';
 import Pruebas from '../Pages/Pruebas/Pruebas';
-
+import { AuthProvider } from '../../context/AuthProvider';
 
 function Application(props) {
   const { history } = props;
   const changeMode = useContext(ThemeContext);
   return (
-    <Dashboard history={history} changeMode={changeMode}>
-      <Switch>
-        <Route exact path="/app" component={BlankPage} />
-        <Route path="/app/dashboard" component={DashboardPage} />
-        <Route path="/app/form" component={Form} />
-        <Route path="/app/table" component={Table} />
-        <Route path="/app/page-list" component={Parent} />
-        <Route path="/app/pages/not-found" component={NotFound} />
-        <Route path="/app/pages/error" component={Error} />
-        <Route path="/app/FormReservaPage" component={FormReservaPage} />
-        <Route
-          path="/app/TarjetaRegistroPage"
-          component={TarjetaRegistroPage}
-        />
-
-        
-        <Route path="/app/Pruebas" component={Pruebas} />
-        <Route path="/app/LoginPage" component={LoginPage} />
-        <Route path="/app/ControlCuenta" component={ControlCuenta} />
-        <Route path="/app/DiarioIngresos" component={DiarioIngresos} />
-        <Route path="/app/FormularioReserva" component={FormularioReserva} />
-        <Route path="/app/FormularioTarjetaRegistro" component={FormTarjetaRegistro} />
-        <Route path="/app/ComandaConsumoFrigobar" component={ComandaConsumoFrigobar} />
-        <Route path="/app/ConsumoCliente" component={ConsumoCliente} />
-        <Route path="/app/ComandaRestaurante" component={ComandaRestaurante} />
-        <Route path="/app/Lavanderia" component={Lavanderia} />
-        <Route path="/app/FormReserva" component={FormReserva} />
-        <Route path="/app/SamplePage" component={SamplePage} />
-        <Route component={NotFound} />
-      </Switch>
-    </Dashboard>
+    <AuthProvider>
+      <Dashboard history={history} changeMode={changeMode}>
+        <Switch>
+          <Route exact path="/app" component={BlankPage} />
+          <Route path="/app/dashboard" component={DashboardPage} />
+          <Route path="/app/form" component={Form} />
+          <Route path="/app/table" component={Table} />
+          <Route path="/app/page-list" component={Parent} />
+          <Route path="/app/pages/not-found" component={NotFound} />
+          <Route path="/app/pages/error" component={Error} />
+          <Route path="/app/FormReservaPage" component={FormReservaPage} />
+          <Route
+            path="/app/TarjetaRegistroPage"
+            component={TarjetaRegistroPage}
+          />
+  
+          
+          <Route path="/app/Pruebas" component={Pruebas} />
+          <Route path="/app/LoginPage" component={LoginPage} />
+          <Route path="/app/ControlCuenta" component={ControlCuenta} />
+          <Route path="/app/DiarioIngresos" component={DiarioIngresos} />
+          <Route path="/app/FormularioReserva" component={FormularioReserva} />
+          <Route path="/app/FormularioTarjetaRegistro" component={FormTarjetaRegistro} />
+          <Route path="/app/ComandaConsumoFrigobar" component={ComandaConsumoFrigobar} />
+          <Route path="/app/ConsumoCliente" component={ConsumoCliente} />
+          <Route path="/app/ComandaRestaurante" component={ComandaRestaurante} />
+          <Route path="/app/Lavanderia" component={Lavanderia} />
+          <Route path="/app/FormReserva" component={FormReserva} />
+          <Route path="/app/SamplePage" component={SamplePage} />
+          <Route component={NotFound} />
+        </Switch>
+      </Dashboard>
+    </AuthProvider>
   );
 }
 
