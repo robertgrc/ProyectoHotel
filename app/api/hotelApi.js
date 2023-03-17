251566ea1c7b48505ab8104efd5cc4dec610fd56
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-03-17 16:44:32 -0400

    Add Pruebas

--- app/api/hotelApi.js
+++ app/api/hotelApi.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
-// import { getEnvVariables } from '../helpers/getEnvVariables';
-// const env = getEnvVariables();
-
 
 const hotelApi = axios.create({
   baseURL: 'http://localhost:4000/api'
 });
 
 // Todo: Configurar interceptores
+hotelApi.interceptors.request.use( config => {
+  config.headers = {
+    ...config.headers,
+    'x-token': localStorage.getItem('token')
+  };
+
+  return config;
+});
 
 export default hotelApi;
--- app/containers/Pages/Pruebas/Pruebas.js
+++ app/containers/Pages/Pruebas/Pruebas.js
@@ -1,13 +1,29 @@
-import React from 'react';
-// import dotenv from 'dotenv';
-// dotenv.config();
 
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { checkAuthToken } from '../../../hooks/checkAuthToken';
 
 const Pruebas = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      try {
+        await checkAuthToken();
+      } catch (error) {
+        // Si hay un error, redireccionamos al usuario a la página de login
+        history.push('/LoginPage');
+      }
+    };
+    isLoggedIn();
+  }, [history]);
+
+  history.push('LoginPage');
+
   return (
     <div>
-      <h1>Pruebas</h1>
-      <h1>{process.env.USUARIO_URL}</h1>
+      <h1>Welcome to your profile</h1>
+      <p>This page is only accessible if you are authenticated</p>
     </div>
   );
 };
