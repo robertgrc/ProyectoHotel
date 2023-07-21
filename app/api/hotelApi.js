1bf706c18ddcf6392b0d3dce82253075d053573f
Marcelo Rodriguez <marcelo.rodriguez@trio.dev>
2023-07-21 17:03:32 -0400

    added env variables to web pack

--- app/api/hotelApi.js
+++ app/api/hotelApi.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
+import { fromJS } from 'immutable';
 
 const hotelApi = axios.create({
-  baseURL: 'http://localhost:4000/api'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 // Todo: Configurar interceptores
-hotelApi.interceptors.request.use( config => {
-  config.headers = {
-    ...config.headers,
+hotelApi.interceptors.request.use(config => {
+  const newConfig = fromJS(config);
+
+  newConfig.headers = {
+    ...newConfig.headers,
     'x-token': localStorage.getItem('token')
   };
 
-  return config;
+  return newConfig;
 });
 
 export default hotelApi;
