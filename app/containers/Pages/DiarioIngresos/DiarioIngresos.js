ffdaa7e9ca928055f3c116091fc122d13e20d33d
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-09-18 17:59:14 -0400

    Agregando llenado de campos en diario Ingresos y Egresos

--- app/containers/Pages/DiarioIngresos/DiarioIngresos.js
+++ app/containers/Pages/DiarioIngresos/DiarioIngresos.js
@@ -1,28 +1,32 @@
 /* eslint-disable react/no-array-index-key */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import hotelApi from '../../../api/hotelApi';
 import DatosDiarioIngresosEgresos from './DatosDiarioIngresosEgresos';
-
-const ingresos = [
-    {
- numero: 1, nombre: 'Venta de producto A', detalle: 'Cliente X', ingresos: 500, egresos: 0
-},
-    {
- numero: 2, nombre: 'Pago de factura Y', detalle: 'Proveedor Z', ingresos: 0, egresos: 300
-},
-    {
- numero: 3, nombre: 'Venta de producto B', detalle: 'Cliente W', ingresos: 1000, egresos: 0
-}
-];
-
-const sumIngresos = ingresos.reduce((acc, curr) => acc + curr.ingresos, 0);
-const sumEgresos = ingresos.reduce((acc, curr) => acc + curr.egresos, 0);
-const saldo = sumIngresos - sumEgresos;
 
 function DiarioIngresos(props) {
   const [initialDiarioIngresosData, setInitialDiarioIngresosData] = useState(null);
+  const [abonos, setAbonos] = useState([]);
+
+  const subtotalIngresos = 0;
+  const subtotalEgresos = 0;
+  const saldo = subtotalIngresos - subtotalEgresos;
+
   function generateUniqueKey(index) {
     return `row-${index}`;
   }
+
+  const idRecepcionista = localStorage.getItem('UidUsuarioLogueado'); // Reemplaza con el ID deseado
+
+  useEffect(() => {
+    hotelApi.get(`editarAbono/recepcionista/${idRecepcionista}`)
+      .then((response) => {
+        console.log('Resultado de la consulta:', response.data);
+        setAbonos(response.data.abonos);
+      })
+      .catch((error) => {
+        console.error('Error en la consulta:', error);
+      });
+  }, [idRecepcionista]);
 
   return (
     <div className="container-abono">
@@ -45,23 +49,31 @@ function DiarioIngresos(props) {
             </tr>
           </thead>
           <tbody>
-            {ingresos.map((ingreso, index) => (
+            {abonos.map((abono, index) => (
               <tr key={generateUniqueKey(index)}>
-                <td>{ingreso.numero}</td>
-                <td>{ingreso.nombre}</td>
-                <td>{ingreso.detalle}</td>
-                <td>{ingreso.ingresos}</td>
-                <td>{ingreso.egresos}</td>
-                <td>{ingreso.ingresos - ingreso.egresos}</td>
+                <td>{index + 1}</td>
+                <td>{abono.detalleAbono}</td>
+                <td>{abono.fechaActual}</td>
+                <td>{abono.abono}</td>
+                <td>0</td>
+                <td>{abono.abono - 0}</td>
               </tr>
             ))}
             <tr>
               <td />
               <td />
+              <td><strong>Subtotales de Ingresos y Egresos </strong></td>
+              <td><strong>{abonos.reduce((acc, curr) => acc + curr.abono, 0)}</strong></td>
+              <td><strong>{abonos.reduce((acc, curr) => acc + 0, 0)}</strong></td>
+              <td />
+            </tr>
+            <tr>
               <td />
               <td />
+              <td><strong>Saldo de Cierre de caja</strong></td>
               <td />
-              <td><strong>{saldo}</strong></td>
+              <td />
+              <td><strong>{abonos.reduce((acc, curr) => acc + curr.abono, 0)}</strong></td>
             </tr>
           </tbody>
         </table>
